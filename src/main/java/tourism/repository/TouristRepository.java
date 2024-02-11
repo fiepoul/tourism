aba07c0c6ba1b8fecd01f6d428b088fb9d7c9dc8
fiepoul <fipo0001@stud.kea.dk>
2024-02-11 15:50:57 +0800

    index.html added, frontpage, links to crud methods

--- src/main/java/tourism/repository/TouristRepository.java
+++ src/main/java/tourism/repository/TouristRepository.java
@@ -13,6 +13,11 @@ public class TouristRepository {
 
     public TouristRepository() {
         attractions.add(new TouristAttraction("flyvergrillen", "et spisested og udsigtspunkt over flyvebanen i Kastrup"));
+        attractions.add(new TouristAttraction("Cisternerne", "En tidligere vandreservoir under Frederiksberg Hill som nu er en kunst udstillingsplads."));
+        attractions.add(new TouristAttraction("BabyZoo på Gavnø Slot", "En lille zoologisk have, der fokuserer på babydyr."));
+        attractions.add(new TouristAttraction("Rabjerg Mile", "En vandrende sandklit nær Skagen."));
+        attractions.add(new TouristAttraction("Verdenskortet ved Klejtrup Sø", "En stor park med et stort kort over verden i skala 1:100.000."));
+        attractions.add(new TouristAttraction("Gravhøjene i Mols Bjerge", "Præhistoriske gravhøje med fantastisk udsigt."));
     }
 
     //CRUD metoder
