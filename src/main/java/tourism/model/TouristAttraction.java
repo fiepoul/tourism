55d6dc263e75b1be1f5c27697f644a2a2d6b9bcf
fiepoul <fipo0001@stud.kea.dk>
2024-02-19 12:51:38 +0800

    java class updated with new attributes, new List of tags and location added to the repository list

--- src/main/java/tourism/model/TouristAttraction.java
+++ src/main/java/tourism/model/TouristAttraction.java
@@ -1,12 +1,19 @@
 package tourism.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TouristAttraction {
     private String name;
     private String description;
+    private String location;
+    private List<String> tags;
 
-    public TouristAttraction(String name, String description) {
+    public TouristAttraction(String name, String description, String location, List<String> tags) {
         this.name = name;
         this.description = description;
+        this.location = location;
+        this.tags = tags;
     }
 
     public String getName() {
--- src/main/java/tourism/repository/TouristRepository.java
+++ src/main/java/tourism/repository/TouristRepository.java
@@ -12,12 +12,12 @@ public class TouristRepository {
     private List<TouristAttraction> attractions = new ArrayList<>();
 
     public TouristRepository() {
-        attractions.add(new TouristAttraction("flyvergrillen", "et spisested og udsigtspunkt over flyvebanen i Kastrup"));
-        attractions.add(new TouristAttraction("Cisternerne", "En tidligere vandreservoir under Frederiksberg Hill som nu er en kunst udstillingsplads."));
-        attractions.add(new TouristAttraction("BabyZoo på Gavnø Slot", "En lille zoologisk have, der fokuserer på babydyr."));
-        attractions.add(new TouristAttraction("Rabjerg Mile", "En vandrende sandklit nær Skagen."));
-        attractions.add(new TouristAttraction("Verdenskortet ved Klejtrup Sø", "En stor park med et stort kort over verden i skala 1:100.000."));
-        attractions.add(new TouristAttraction("Gravhøjene i Mols Bjerge", "Præhistoriske gravhøje med fantastisk udsigt."));
+        attractions.add(new TouristAttraction("flyvergrillen", "et spisested og udsigtspunkt over flyvebanen i Kastrup", "Amager", List.of("mad", "udsigt", "eventyr")));
+        attractions.add(new TouristAttraction("Cisternerne", "En tidligere vandreservoir under Frederiksberg Hill som nu er en kunst udstillingsplads.", "Frederiksberg", List.of("kunst", "natur")));
+        attractions.add(new TouristAttraction("BabyZoo på Gavnø Slot", "En lille zoologisk have, der fokuserer på babydyr.", "Gavnø", List.of("dyr", "slotbesøg")));
+        attractions.add(new TouristAttraction("Råbjerg Mile", "En vandrende sandklit nær Skagen.", "Råbjerg Mile", List.of("natur", "vandretur")));
+        attractions.add(new TouristAttraction("Verdenskortet ved Klejtrup Sø", "En stor park med et stort kort over verden i skala 1:100.000.", "Klejtrup", List.of("natur", "eventyr")));
+        attractions.add(new TouristAttraction("Gravhøjene i Mols Bjerge", "Præhistoriske gravhøje med fantastisk udsigt.", "Mols Bjerge", List.of("historisk", "udsigt", "natur")));
     }
 
     //CRUD metoder
