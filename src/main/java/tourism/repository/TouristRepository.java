e379217e285eb16d0db402fb3bcff4d11b2b77bc
fiepoul <fipo0001@stud.kea.dk>
2024-02-24 17:34:33 +0800

    more attraction added to welcome

--- src/main/java/tourism/repository/TouristRepository.java
+++ src/main/java/tourism/repository/TouristRepository.java
@@ -16,10 +16,10 @@ public class TouristRepository {
     public TouristRepository() {
         attractions.add(new TouristAttraction("flyvergrillen", "et spisested og udsigtspunkt over flyvebanen i Kastrup", "Amager", List.of(AttractionTag.MAD_DRIKKE, AttractionTag.HISTORISK, AttractionTag.UDSIGT)));
         attractions.add(new TouristAttraction("Cisternerne", "En tidligere vandreservoir under Frederiksberg Hill som nu er en kunst udstillingsplads.", "Frederiksberg", List.of(AttractionTag.EVENTYRLIG, AttractionTag.KUNST, AttractionTag.KULTUR)));
-        attractions.add(new TouristAttraction("BabyZoo på Gavnø Slot", "En lille zoologisk have, der fokuserer på babydyr.", "Gavnø", List.of(AttractionTag.FAMILIEVENLIG, AttractionTag.EVENTYRLIG)));
-        attractions.add(new TouristAttraction("Råbjerg Mile", "En vandrende sandklit nær Skagen.", "Råbjerg Mile", List.of(AttractionTag.NATUR, AttractionTag.UDSIGT)));
-        attractions.add(new TouristAttraction("Verdenskortet ved Klejtrup Sø", "En stor park med et stort kort over verden i skala 1:100.000.", "Klejtrup", List.of(AttractionTag.NATUR, AttractionTag.EVENTYRLIG)));
-        attractions.add(new TouristAttraction("Gravhøjene i Mols Bjerge", "Præhistoriske gravhøje med fantastisk udsigt.", "Mols Bjerge", List.of(AttractionTag.HISTORISK, AttractionTag.UDSIGT, AttractionTag.NATUR)));
+        attractions.add(new TouristAttraction("BabyZoo på Gavnoe Slot", "En lille zoologisk have, der fokuserer på babydyr.", "Gavnø", List.of(AttractionTag.FAMILIEVENLIG, AttractionTag.EVENTYRLIG)));
+        attractions.add(new TouristAttraction("Raabjerg_Mile", "En vandrende sandklit nær Skagen.", "Råbjerg Mile", List.of(AttractionTag.NATUR, AttractionTag.UDSIGT)));
+        attractions.add(new TouristAttraction("Verdenskortet ved Klejtrup Soe", "En stor park med et stort kort over verden i skala 1:100.000.", "Klejtrup", List.of(AttractionTag.NATUR, AttractionTag.EVENTYRLIG)));
+        attractions.add(new TouristAttraction("Mols_Bjerge", "Præhistoriske gravhøje med fantastisk udsigt.", "Mols Bjerge", List.of(AttractionTag.HISTORISK, AttractionTag.UDSIGT, AttractionTag.NATUR)));
     }
 
     public List<String> getLocations() {
